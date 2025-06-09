7363b9b091b53487eb055849b4bb0e309c3c78fa
RifatHasan232 <rifat.m360ict@gmail.com>
2025-06-09 23:51:08 +0600

    theme data

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors { 
+  static const primaryColor = Colors; // Purple
+  static const secondaryColor = 0xFF03DAC6; // Teal
+  static const backgroundColor = 0xFFFFFFFF; // White
+  static const errorColor = 0xFFB00020; // Red
+  static const textColor = 0xFF000000; // Black
+  static const accentColor = 0xFFFFC107; // Amber
+}
